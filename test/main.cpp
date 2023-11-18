8161a641b0fe8d00c3e58ec860d8df57688c804c
Brian Carroll <brian.carroll.ireland@gmail.com>
2023-11-18 19:35:56 +0000

    Move .cpp files to test directory and combine them

--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,4 @@
+#include "str.h"
 #include "alloc.h"
 #include <cstdlib>
 #include <cstdio>
@@ -5,26 +6,33 @@
 
 void *roc_alloc(size_t size, uint32_t _alignment)
 {
-  return malloc(size);
+    return malloc(size);
 }
 
 void *roc_realloc(void *ptr, size_t new_size, size_t _old_size, size_t _alignment)
 {
-  return realloc(ptr, new_size);
+    return realloc(ptr, new_size);
 }
 
 void roc_dealloc(void *ptr, uint32_t _alignment)
 {
-  free(ptr);
+    free(ptr);
 }
 
 void roc_panic(const char *message, uint32_t tag_id)
 {
-  fprintf(stderr, "%s", message);
-  exit(tag_id);
+    fprintf(stderr, "%s", message);
+    exit(tag_id);
 }
 
 void *roc_memset(void *ptr, int value, size_t num_bytes)
 {
-  return memset(ptr, value, num_bytes);
+    return memset(ptr, value, num_bytes);
+}
+
+int main()
+{
+    Roc::Str s("hello");
+    printf("String length is %zd\n", s.length());
+    return 0;
 }
