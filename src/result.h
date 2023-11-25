bb04de43a703320735d0393481411a0e35c72da0
Brian Carroll <brian.carroll.ireland@gmail.com>
2023-11-25 15:43:23 +0000

    Trying to fix compiler errors in Result

--- src/result.h
+++ src/result.h
@@ -4,30 +4,64 @@
 
 namespace Roc
 {
+
     template <typename O, typename E>
     class Result : Value
     {
-        union Payload
+        union ResultPayload
         {
             O ok;
             E err;
+
+            ResultPayload(bool is_ok)
+            {
+                if (is_ok)
+                    ok = O();
+                else
+                    err = E();
+            }
+
+            ResultPayload(const O &other)
+            {
+                ok = other;
+            }
+
+            // This is a problem if O and E are the same type            
+            ResultPayload(const E &other)
+            {
+                err = other;
+            }
+
+            ~ResultPayload()
+            {
+            }
         } m_payload;
         bool m_is_ok;
 
     public:
+        Result(bool is_ok)
+            : m_payload(is_ok), m_is_ok(is_ok)
+        {
+        }
+
+        Result(const Result &other)
+        {
+            m_is_ok = other.m_is_ok;
+            m_payload = other.m_payload;
+            rc_increment();
+        }
+
         static Result make_ok(O val)
         {
-            Result r;
+            Result r(true);
             r.m_payload.ok = val;
-            r.m_is_ok = true;
             return r;
         }
 
         static Result make_err(E val)
         {
-            Result r;
+            Result r(false);
             r.m_payload.err = val;
-            r.m_is_ok = false;
             return r;
         }
 
@@ -36,18 +70,26 @@ namespace Roc
             return m_is_ok;
         }
 
-        O* ok() const
+        O *ok() const
         {
-            return m_is_ok ? &m_payload->ok : NULL;
+            return m_is_ok ? &m_payload.ok : NULL;
         }
 
-        E* err() const
+        E *err() const
         {
-            return m_is_ok ? NULL : &m_payload->err;
+            return m_is_ok ? NULL : &m_payload.err;
         }
 
         ~Result()
         {
+            if (m_is_ok)
+            {
+                m_payload.ok.~O();
+            }
+            else
+            {
+                m_payload.err.~E();
+            }
             rc_decrement();
         }
 
