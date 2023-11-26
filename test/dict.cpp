db1209b72dc0465aefbb273eb52e28203dc14eb0
Brian Carroll <brian.carroll.ireland@gmail.com>
2023-11-26 22:50:11 +0000

    Make sure Dict tests include 'value first' and 'key first' variants

--- test/dict.cpp
+++ test/dict.cpp
@@ -4,10 +4,10 @@
 namespace Roc
 {
 
-    DEFINE_TEST_G(I64Str, Dict)
+    DEFINE_TEST_G(I16Str, Dict)
     {
-        Dict<I64, Str> dict(5);
-        I64 ints[5] = {I64(1), I64(2), I64(3), I64(4), I64(5)};
+        Dict<I16, Str> dict(5);
+        I16 ints[5] = {I16(1), I16(2), I16(3), I16(4), I16(5)};
         Str strs[5] = {Str("1"), Str("2"), Str("3"), Str("4"), Str("5")};
         for (size_t i = 0; i < 5; i++)
         {
@@ -26,10 +26,10 @@ namespace Roc
         TEST_EQ(dict.capacity(), 5);
     }
 
-    DEFINE_TEST_G(StrI64, Dict)
+    DEFINE_TEST_G(StrI16, Dict)
     {
-        Dict<Str, I64> dict(5);
-        I64 ints[5] = {I64(1), I64(2), I64(3), I64(4), I64(5)};
+        Dict<Str, I16> dict(5);
+        I16 ints[5] = {I16(1), I16(2), I16(3), I16(4), I16(5)};
         Str strs[5] = {Str("1"), Str("2"), Str("3"), Str("4"), Str("5")};
         for (size_t i = 0; i < 5; i++)
         {
@@ -37,24 +37,24 @@ namespace Roc
         }
         for (size_t i = 0; i < 5; i++)
         {
-            I64 *x = dict.get(&strs[i]);
+            I16 *x = dict.get(&strs[i]);
             TEST_EQ(*x, ints[i]);
         }
         TEST_EQ(dict.length(), 5);
         TEST_EQ(dict.capacity(), 5);
     }
 
-    DEFINE_TEST_G(I64I64, Dict)
+    DEFINE_TEST_G(I16I16, Dict)
     {
-        Dict<I64, I64> dict(5);
-        I64 ints[5] = {I64(1), I64(2), I64(3), I64(4), I64(5)};
+        Dict<I16, I16> dict(5);
+        I16 ints[5] = {I16(1), I16(2), I16(3), I16(4), I16(5)};
         for (size_t i = 0; i < 5; i++)
         {
             dict.insert(ints[i], ints[i]);
         }
         for (size_t i = 0; i < 5; i++)
         {
-            I64 *x = dict.get(&ints[i]);
+            I16 *x = dict.get(&ints[i]);
             TEST_EQ(*x, ints[i]);
         }
         TEST_EQ(dict.length(), 5);
